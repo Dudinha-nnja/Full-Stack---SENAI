0a26e142f7d256454fb6dc5e3294df0733364794
Dudinha-nnja <eduardarod04@gmail.com>
2024-11-14 17:22:04 -0300

    v1
    antecessor, sucessor, divisão com js

---
+++ Atividade JavaScript/script/index.js
@@ -0,0 +1,18 @@
+//var n1 = prompt ("Numero total de cliente :")
+//var n2 = prompt ("Valor total da conta: ")
+
+
+//var soma  = Number(n1) + Number(n2)
+//var div = Number (n1) / Number (n2)
+
+//alert ("A conta dividida é :" +div)
+
+//antecessor e sucessor
+
+var n1 = prompt ("Digite um número :")
+var antecessor= Number (n1) -1 
+alert ("O antecessor é : " +antecessor)
+
+var sucessor = Number (n1) +1
+alert ("O sucessor do número é:" +sucessor)
+
